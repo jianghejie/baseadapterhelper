5cbfc3b26654ab54c1233cdf021f9bcfdeb09e7d
hejie <jianghejie888@163.com>
2015-11-10 18:25:19 +0800

    delete  useless code

--- sample/src/main/java/com/jcodecraeer/sample/MainActivity.java
+++ sample/src/main/java/com/jcodecraeer/sample/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.joanzapata.android.recyclerview.BaseAdapterHelper;
 import com.joanzapata.android.recyclerview.QuickAdapter;
@@ -46,27 +44,5 @@ public class MainActivity extends AppCompatActivity {
             temp.setAvatar("http://www.jcodecraeer.com/member/templets/images/dfboy.png");
             mAdapter.addItem(temp);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
